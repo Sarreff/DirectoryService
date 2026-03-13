a586ecdf6d78fcf0e27030cb2043d3557ce7015a
Sarreff <Sarreff@yandex.ru>
2026-03-13 23:20:22 +0300

    Написал интеграционные тесты для обновления локаций у департамента.

---
+++ DirectoryService/tests/DirectoryService.IntegrationTests/Departments/UpdateLocations/DepartmentAssertions.cs
@@ -0,0 +1,84 @@
+﻿using CSharpFunctionalExtensions;
+using DirectoryService.Domain.Departments.ValueObjects;
+using DirectoryService.Domain.Locations.ValueObjects;
+using DirectoryService.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace DirectoryService.IntegrationTests.Departments;
+
+public partial class DepartmentAssertions
+{
+    // Успешное обновление локаций у департамента
+    public async Task DepartmentUpdated(
+        Result<Guid, Errors> result,
+        int expectedLocationCount,
+        LocationId[] oldLocIds,
+        LocationId[] newLocIds,
+        CancellationToken cancellationToken)
+    {
+        Assert.True(result.IsSuccess);
+        Assert.NotEqual(Guid.Empty, result.Value);
+
+        await _db.ExecuteInDb(async dbContext =>
+        {
+            var department = await dbContext.Departments
+                .Include(d => d.DepartmentLocations)
+                .FirstAsync(d => d.Id == new DepartmentId(result.Value), cancellationToken);
+
+            Assert.NotNull(department);
+            Assert.Equal(result.Value, department.Id.Value);
+
+            var oldLocationIds = oldLocIds
+                .Select(l => l.Value)
+                .ToHashSet();
+
+            var actualLocationIds = department.DepartmentLocations
+                .Select(dl => dl.LocationId.Value)
+                .ToHashSet();
+
+            var expectedLocationIds = newLocIds
+                .Select(l => l.Value)
+                .ToHashSet();
+
+            Assert.Equal(expectedLocationCount, actualLocationIds.Count);
+            Assert.Equal(expectedLocationIds, actualLocationIds);
+            Assert.NotEqual(oldLocationIds, actualLocationIds);
+        });
+    }
+
+    // Неуспешное обновление локаций у департамента
+    public async Task DepartmentDidNotUpdated(
+        Result<Guid, Errors> result,
+        DepartmentId departmentId,
+        int expectedLocationCount,
+        LocationId[] oldLocIds,
+        ErrorType type,
+        DateTime originalUpdatedAt,
+        CancellationToken cancellationToken)
+    {
+        Assert.True(result.IsFailure);
+        Assert.NotEmpty(result.Error);
+
+        Assert.Contains(result.Error, e => e.Type == type);
+
+        await _db.ExecuteInDb(async dbContext =>
+        {
+            var department = await dbContext.Departments
+                .Include(d => d.DepartmentLocations)
+                .FirstAsync(d => d.Id == departmentId, cancellationToken);
+
+            var expectedLocationIds = oldLocIds
+                .Select(l => l.Value)
+                .ToHashSet();
+
+            var actualLocationIds = department.DepartmentLocations
+                .Select(dl => dl.LocationId.Value)
+                .ToHashSet();
+
+            Assert.Equal(expectedLocationCount, actualLocationIds.Count);
+            Assert.Equal(expectedLocationIds, actualLocationIds);
+
+            Assert.Equal(originalUpdatedAt, department.UpdatedAt);
+        });
+    }
+}
---
+++ DirectoryService/tests/DirectoryService.IntegrationTests/Departments/UpdateLocations/UpdateDepartmentLocationsTests.cs
@@ -0,0 +1,194 @@
+﻿using CSharpFunctionalExtensions;
+using DirectoryService.Application.Departments.UpdateDepartment;
+using DirectoryService.Contracts.Departments;
+using DirectoryService.Domain.Departments;
+using DirectoryService.Domain.Locations.ValueObjects;
+using DirectoryService.IntegrationTests.Infrastructure;
+using DirectoryService.Shared;
+
+namespace DirectoryService.IntegrationTests.Departments.UpdateLocations;
+
+public class UpdateDepartmentLocationsTests : DirectoryBaseTests
+{
+    public UpdateDepartmentLocationsTests(DirectoryTestWebFactory factory)
+        : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task UpdateDepartment_with_valid_location_should_succeed()
+    {
+        // arrange
+        LocationId locationId = await Data.CreateValidLocation();
+
+        Department department = await Data
+            .CreateValidParentDepartment(
+                "Department",
+                "department",
+                [locationId]);
+
+        LocationId locationIdForUpdate = await Data.CreateValidLocation();
+
+        const int expectedLocationCount = 1;
+
+        CancellationToken cancellationToken = CancellationToken.None;
+
+        // act
+        Result<Guid, Errors> result =
+            await ExecuteHandler<UpdateDepartmentLocationsHandler, Result<Guid, Errors>>(
+                (sut) =>
+            {
+                var command =
+                    new UpdateDepartmentLocationsCommand(
+                        department.Id.Value,
+                        new UpdateDepartmentLocationsRequest([locationIdForUpdate.Value]));
+
+                return sut.Handle(command, cancellationToken);
+            });
+
+        // assert
+        await AssertDb.DepartmentUpdated(
+            result,
+            expectedLocationCount,
+            [locationId],
+            [locationIdForUpdate],
+            cancellationToken);
+    }
+
+    [Fact]
+    public async Task UpdateDepartment_with_several_valid_locations_should_succeed()
+    {
+        // arrange
+        LocationId locationId1 = await Data.CreateValidLocation();
+        LocationId locationId2 = await Data.CreateValidLocation();
+
+        Department department = await Data
+            .CreateValidParentDepartment(
+                "Department",
+                "department",
+                [locationId1, locationId2]);
+
+        LocationId locationIdForUpdate1 = await Data.CreateValidLocation();
+        LocationId locationIdForUpdate2 = await Data.CreateValidLocation();
+        LocationId locationIdForUpdate3 = await Data.CreateValidLocation();
+        LocationId locationIdForUpdate4 = await Data.CreateValidLocation();
+
+        const int expectedLocationCount = 4;
+
+        CancellationToken cancellationToken = CancellationToken.None;
+
+        // act
+        Result<Guid, Errors> result =
+            await ExecuteHandler<UpdateDepartmentLocationsHandler, Result<Guid, Errors>>(
+                (sut) =>
+            {
+                var command =
+                    new UpdateDepartmentLocationsCommand(
+                        department.Id.Value,
+                        new UpdateDepartmentLocationsRequest([
+                            locationIdForUpdate1.Value,
+                            locationIdForUpdate2.Value,
+                            locationIdForUpdate3.Value,
+                            locationIdForUpdate4.Value
+                        ]));
+
+                return sut.Handle(command, cancellationToken);
+            });
+
+        // assert
+        await AssertDb.DepartmentUpdated(
+            result,
+            expectedLocationCount,
+            [locationId1, locationId2],
+            [locationIdForUpdate1, locationIdForUpdate2, locationIdForUpdate3, locationIdForUpdate4],
+            cancellationToken);
+    }
+
+    [Fact]
+    public async Task UpdateDepartment_with_inactive_location_should_return_not_found()
+    {
+        // arrange
+        LocationId locationId = await Data.CreateValidLocation();
+
+        Department department = await Data
+            .CreateValidParentDepartment(
+                "Department",
+                "department",
+                [locationId]);
+
+        DateTime originalDepartmentUpdatedAt = await Data.GetDepartmentUpdatedAt(department.Id);
+
+        LocationId locationIdForUpdate = await Data.CreateInactiveLocation();
+
+        const int expectedLocationCount = 1;
+
+        CancellationToken cancellationToken = CancellationToken.None;
+
+        // act
+        Result<Guid, Errors> result =
+            await ExecuteHandler<UpdateDepartmentLocationsHandler, Result<Guid, Errors>>(
+                (sut) =>
+            {
+                var command =
+                    new UpdateDepartmentLocationsCommand(
+                        department.Id.Value,
+                        new UpdateDepartmentLocationsRequest([locationIdForUpdate.Value]));
+
+                return sut.Handle(command, cancellationToken);
+            });
+
+        // assert
+        await AssertDb.DepartmentDidNotUpdated(
+            result,
+            department.Id,
+            expectedLocationCount,
+            [locationId],
+            ErrorType.NOT_FOUND,
+            originalDepartmentUpdatedAt,
+            cancellationToken);
+    }
+
+    [Fact]
+    public async Task UpdateDepartment_location_not_found_should_return_not_found()
+    {
+        // arrange
+        LocationId locationId = await Data.CreateValidLocation();
+
+        Department department = await Data
+            .CreateValidParentDepartment(
+                "Department",
+                "department",
+                [locationId]);
+
+        DateTime originalDepartmentUpdatedAt = await Data.GetDepartmentUpdatedAt(department.Id);
+
+        LocationId locationIdForUpdate = Data.CreateNotExistLocation();
+
+        const int expectedLocationCount = 1;
+
+        CancellationToken cancellationToken = CancellationToken.None;
+
+        // act
+        Result<Guid, Errors> result =
+            await ExecuteHandler<UpdateDepartmentLocationsHandler, Result<Guid, Errors>>(
+                (sut) =>
+            {
+                var command =
+                    new UpdateDepartmentLocationsCommand(
+                        department.Id.Value,
+                        new UpdateDepartmentLocationsRequest([locationIdForUpdate.Value]));
+
+                return sut.Handle(command, cancellationToken);
+            });
+
+        // assert
+        await AssertDb.DepartmentDidNotUpdated(
+            result,
+            department.Id,
+            expectedLocationCount,
+            [locationId],
+            ErrorType.NOT_FOUND,
+            originalDepartmentUpdatedAt,
+            cancellationToken);
+    }
+}
